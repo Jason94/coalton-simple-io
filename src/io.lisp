4c1ee38b2ea7609d9fed509dfb17d5833179a00f
Jason Walker <Jason0@pm.me>
2025-09-12 09:56:33 -0400

    Inline everything

--- src/io.lisp
+++ src/io.lisp
@@ -29,24 +29,29 @@ Example:
   (define-type (IO :a)
     (IO% (Unit -> :a)))
 
+  (inline)
   (declare run! (IO :a -> :a))
   (define (run! (IO% funit->a))
     (funit->a))
 
   (define-instance (Functor IO)
+    (inline)
     (define (map fb->c (IO% funit->b))
       (IO%
         (fn ()
           (fb->c (funit->b))))))
 
   (define-instance (Applicative IO)
+    (inline)
     (define pure (compose IO% const))
+    (inline)
     (define (liftA2 fa->b->c (IO% f->a) (IO% f->b))
       (IO%
         (fn ()
           (fa->b->c (f->a) (f->b))))))
 
   (define-instance (Monad IO)
+    (inline)
     (define (>>= (IO% f->a) fa->io-b)
       (IO%
         (fn ()
